b25a30b30f686a6826215aa54df56ec57369444a
Salman-Kiwan <salmankiwan889@gmail.com>
2025-09-10 23:28:43 +0300

    debugging viteBase

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'Movie-React-App',
+  base:'/Movie-React-App/',
   plugins: [react(),tailwindcss()],
 })
